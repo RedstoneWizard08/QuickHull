03816685c0308d5dd9a0f9b5e32984a03f7a7d59
Dannie226 <77417944+Dannie226@users.noreply.github.com>
2022-05-06 18:58:34 -0400

    JSDoc-ify, rename parameter
    Added JSDoc comments, and renamed vector parameter to vectorArray

--- src/vector.ts
+++ src/vector.ts
@@ -4,15 +4,25 @@ export interface Vector3 {
     z: number;
 }
 
+/**
+* Converts a vector to a number array
+* @param {Vector3} vector
+* @returns {number[]}
+**/
 export const vectorToNumber = (vector: Vector3) => [
     vector.x,
     vector.y,
     vector.z,
 ];
 
-export const vectorArrayToNumberArray = (vector: (Vector3 | number[])[]) => {
+/**
+* Vector array to number array conversion
+* @param {(Vector3 | number[])[]} vectorArray
+* @returns {number[][]}
+**/
+export const vectorArrayToNumberArray = (vectorArray: (Vector3 | number[])[]) => {
     const vectors: number[][] = [];
-    vector.forEach((v) => {
+    vectorArray.forEach((v) => {
         if ("x" in v) {
             vectors.push([v.x, v.y, v.z]);
         } else {
